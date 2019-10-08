1b968bc885bacee0bc62e416107040c1da928e5d
1185491736@qq.com <1185491736@qq.com>
2019-10-08 23:52:02 +0800

    inite

---
+++ server/cloud-functions/he-weather/index.js
@@ -0,0 +1,37 @@
+const API_URL = 'https://free-api.heweather.net/s6/weather'
+const request = require('request')
+
+/*<jdists import="../../inline/utils.js" />*/
+
+/*<remove>*/
+const $ = require('../../inline/utils')
+/*</remove>*/
+
+exports.main = async (event) => {
+  const {lat,lon} = event
+  let location = `${lat},${lon}`
+  let params = {
+    location,
+    key: 'XXX' //和风天气中应用的密钥
+  }
+  let query = []
+  for (let i in params) {
+    query.push(`${i}=${encodeURIComponent(params[i])}`)
+  }
+  let url = API_URL + '?' + query.join('&')
+  // console.log(url)
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        reject(error)
+      } else {
+        try {
+          let rs = $.handlerData(JSON.parse(body))
+          resolve(rs)
+        } catch (e) {
+          reject(e)
+        }
+      }
+    })
+  })
+}
